3ee0dd1d710a9b0540d8d722385aa6ba485596b2
AhmedAlsikely <ahmed.alsikely773@gmail.com>
2024-01-30 19:27:37 +0200

    V1.5.0

--- classes/Led/led.cpp
+++ classes/Led/led.cpp
@@ -4,12 +4,31 @@ LED::LED(int pin_num): gpio_num_i(pin_num){
     gpio_num_s = std::to_string(gpio_num_i);
     dir_Path = dir_Path + gpio_num_s;
 
-    if (std::filesystem::exists(dir_Path) && std::filesystem::is_directory(dir_Path)) {
+    if (std::filesystem::exists(dir_Path) && std::filesystem::is_directory(dir_Path)) 
+    {
         std::cout << "File exists." << std::endl;
-    } else {
+
+    }
+    else 
+    {
         std::cout << "File does not exist." << std::endl;
         std::cout << "will create file now." << std::endl;
         std::ofstream outputFile(PATH_EXPORT);
-        
+        if (!outputFile.is_open()) 
+        {
+        std::cerr << "Error opening file: " << PATH_EXPORT << std::endl;
+        }
+        else
+        {
+            outputFile << gpio_num_s ;
+            outputFile.close();
+            std::cout << "Data has been written to " << PATH_EXPORT << std::endl;
+
+
+        }
     }
+}
+
+Std_Return LED::Set_Direction(){
+    
 }
