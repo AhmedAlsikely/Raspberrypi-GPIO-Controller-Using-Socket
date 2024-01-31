a0ff615791d0c1ff462b64e316e515f25f192266
AhmedAlsikely <ahmed.alsikely773@gmail.com>
2024-01-31 17:29:06 +0200

    V2.7.0

--- classes/command/command.hpp
+++ classes/command/command.hpp
@@ -6,13 +6,6 @@
 
 class Command {
     public:
-    
-        const char *youtube = "firefox https://www.youtube.com";
-        const char *cal = "gnome-calculator";
-        const char *vscode = "code ~/codes";
-        const char *poweroff ="systemctl poweroff";
-        const char *terminal ="gnome-terminal";
-
     Std_Return executeCmdLine(std::string commaned);
     private:
 
